71fce12c895c6de702d7479d1f4fb9647f862980
Bahster1 <slybahwalsh27@gmail.com>
2020-04-29 12:46:20 -0700

    Added fractal generation methods using the Turtle library.

---
+++ PythonProject1/src/main.py
@@ -0,0 +1,10 @@
+from PythonProject1.fractals import koch_snowflake, apollonian_gasket, newton_fractals
+
+# The Newton Fractal is the most interesting one in that it can generate a new kind of fractal depending on the
+# equation you enter for its main function. I have found that increasing the power of 'z' beyond 4 causes
+# division issues.
+#
+# Images are stored in the 'src' directory.
+#
+# All of these fractals are not mine, and I did not make a single one. The code can be found on the following link:
+# https://prettymathpics.com/fractal-generating-python-programs/
